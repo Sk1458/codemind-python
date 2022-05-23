2adf1ef6d21e26ea196bd02573388529df37abca
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-05-23 18:34:49 +0530

    def rev(n):     res=0     while n>0:         rem=n%10         res=res*10+rem         n=n//10     return res
    x=int(input())
    X=x*x
    y=rev(x)
    Y=y*y
    if X==rev(Y):
        print("True")
    else:
        print("False")

---
+++ Adam_number.py
@@ -0,0 +1,17 @@
+def rev(n):
+    res=0
+    while n>0:
+        rem=n%10
+        res=res*10+rem
+        n=n//10
+    return res    
+
+x=int(input())
+X=x*x
+y=rev(x)
+Y=y*y
+if X==rev(Y):
+    print("True")
+else:
+    print("False")
+
