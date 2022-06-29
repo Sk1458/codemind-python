6233092e031a266f5430166056008cb4b1983939
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-06-29 18:34:02 +0530

    def prime(n):     while n:         c = 0         for i in range(2,n//2):             if n%i==0:                 c+=1         if c==0:             return n         n+=1
    k = int(input())
    l = int(input())
    print(prime(k+l+1)-(k+l))

---
+++ Battle_of_Primes.py
@@ -0,0 +1,13 @@
+def prime(n):
+    while n:
+        c = 0
+        for i in range(2,n//2):
+            if n%i==0:
+                c+=1
+        if c==0:
+            return n
+        n+=1
+        
+k = int(input())
+l = int(input())
+print(prime(k+l+1)-(k+l))
