48a3f7ddd4e77f4b68ff73c7e82f9238ad0ca641
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-07-01 21:06:44 +0530

    k=int(input()) p=0 for i in range(1,k):     if k%i==0:         p+=i if p==k:     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,11 +1,9 @@
-x=int(input())
-c=0
-for i in range(1,x):
-    if x%i==0:
-        c+=i
-if x==c:
+k=int(input())
+p=0
+for i in range(1,k):
+    if k%i==0:
+        p+=i
+if p==k:
     print("True")
 else:
-    print("False")
-    
-        
+    print("False")    
