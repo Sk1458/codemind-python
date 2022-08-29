af15fc88b8a68517bab2b45c372bc70d6841b769
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-08-29 15:15:16 +0530

    #program to return index position of unique character in a given string
    s = input()
    a = s #duplicating the string
    for i in s:
        c=0     # to check the count of a character in string 
        I=-1    # to have a note of j pointer's index position in for loop 
        for j in a:
            I+=1
            if i==j:
                c+=1
                if c==1:
                    st = I # it stores the index postion
                if c>1:
                    st=0   
        if c==1:
            print(st)
            break
    if c>1:
        print(-1)

---
+++ First_Unique_Character_in_a_String.py
@@ -0,0 +1,21 @@
+#program to return index position of unique character in a given string
+
+s = input()
+a = s #duplicating the string
+for i in s:
+    c=0     # to check the count of a character in string 
+    I=-1    # to have a note of j pointer's index position in for loop 
+    for j in a:
+        I+=1
+        if i==j:
+            c+=1
+            if c==1:
+                st = I # it stores the index postion
+            if c>1:
+                st=0   
+    if c==1:
+        print(st)
+        break
+if c>1:
+    print(-1)
+
