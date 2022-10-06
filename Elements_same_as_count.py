b0b0b51e0759305821e3a38a6f098a068f3755f9
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-10-06 11:39:37 +0530

    n = int(input()) arr = list(map(int,input().split())) c = 0 for i in sorted(set(arr),key=arr.index):     if arr.count(i)==i:         print(i,end=" ")         c = 1 if c==0:     print("-1")

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,8 +1,8 @@
 n = int(input())
-a = list(map(int,input().split()))
+arr = list(map(int,input().split()))
 c = 0
-for i in sorted(set(a),key=a.index):
-    if a.count(i)==i:
+for i in sorted(set(arr),key=arr.index):
+    if arr.count(i)==i:
         print(i,end=" ")
         c = 1
 if c==0:
